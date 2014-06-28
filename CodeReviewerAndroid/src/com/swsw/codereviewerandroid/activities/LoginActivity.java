7ddbf74485d47c06a32b4ac0a232bd72be09e7a8
VinsonCai <wensheng1816@gmail.com>
2014-06-29 00:23:17 +0800

    login

--- CodeReviewerAndroid/src/com/swsw/codereviewerandroid/activities/LoginActivity.java
+++ CodeReviewerAndroid/src/com/swsw/codereviewerandroid/activities/LoginActivity.java
@@ -5,6 +5,12 @@ package com.swsw.codereviewerandroid.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.swsw.codereviewerandroid.R;
 
 /**
  * @author VinsonCai
@@ -12,8 +18,43 @@ import android.os.Bundle;
  */
 public class LoginActivity extends Activity {
 
+	private Button mLoginButton;
+	private EditText mUserNameEditText;
+	private EditText mPasswordEditText;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.login);
+		initViews();
+	}
+
+	private void initViews() {
+		mLoginButton = (Button) findViewById(R.id.login_go_button);
+		mLoginButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				doLogin();
+			}
+		});
+
+		mUserNameEditText = (EditText) findViewById(R.id.login_user_name_editText);
+
+		mPasswordEditText = (EditText) findViewById(R.id.login_pwd_editText);
+	}
+
+	private void doLogin() {
+		String userName = mUserNameEditText.getText().toString();
+		String pwd = mPasswordEditText.getText().toString();
+		if (isValid(userName, pwd)) {
+
+		}
+	}
+
+	private boolean isValid(String userName, String pwd) {
+
+		return true;
 	}
 }
